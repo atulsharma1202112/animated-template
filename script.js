193b5f96279385fdd9c42555c34fc304bceb0993
Atul Sharma <atulsharma1202112@gmail.com>
2020-11-13 15:44:55 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+AOS.init({
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    delay: 200, // values from 0 to 3000, with step 50ms
+    duration: 1500, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+});
